e698ceb581ff2fd5b86abffd410d69ea4f85bdc7
Tyler Menezes <tylermenezes@codeday.org>
2021-08-26 11:33:12 -0400

    Add subscribe message

--- subscribe.js
+++ subscribe.js
@@ -1,4 +1,5 @@
 (async () => {
+  await require('./src/twilio').sendMessage(process.argv[2], `You're now subscribed for EnforcerNews text messages!`);
   await require('./src/db').subscribe(process.argv[2]);
   process.exit();
 })();
